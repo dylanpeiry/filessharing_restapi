3bfaf9b8a1837f122eeed526dbc83ad182c5e9b9
dylanpeiry <dylan.peiry@gmail.com>
2019-09-02 11:15:29 +0200

    Init repo with project

---
+++ app/Http/Controllers/FilesController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class FilesController extends Controller
+{
+    public $successStatus = 200;
+    public function get(Request $request)
+    {
+
+
+        $files = [
+            [
+                'id' => 1,
+                'storedFileName' => 'test',
+                'fileName' => 'test.txt',
+                'size' => 200,
+                'type' => '.txt',
+                'status' => 1,
+                'idOwner' => 1,
+            ],
+            [
+                'id' => 2,
+                'storedFileName' => 'test2',
+                'fileName' => 'test2.txt',
+                'size' => 300,
+                'type' => '.txt',
+                'status' => 0,
+                'idOwner' => 1,
+            ]
+        ];
+        $user = Auth::user();
+        return response()->json(['success' => $files], $this->successStatus);
+    }
+}
